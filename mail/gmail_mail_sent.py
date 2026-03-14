eb4ed7d8ae6e17256dc037c02087bfe8d3fe1056
Akshay Soni <akshaysoni460@gmail.com>
2026-03-14 22:02:39 +0530

    add smtp sever and email send codez

--- mail/gmail_mail_sent.py
+++ mail/gmail_mail_sent.py
@@ -2,6 +2,9 @@ from mail.base_mail import SentMail
 from models.response_model import MailHandlerResponse
 from models.request_model import MailHandlerRequest
 
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 class GmailMailSent(SentMail):
 
@@ -9,11 +12,45 @@ class GmailMailSent(SentMail):
         self.user_name = user_name
         self.user_email = user_email
 
+
     def sent_mail(self, data: MailHandlerRequest):
         print(f"Sending Message {data.message} to user {data.receiver_email_address}")
-        return MailHandlerResponse(
-            success=True,
-            code=200,
-            status="Success",
-            error_message=""
-        )
+        ## Logic to sent
+        try:
+            ## Code
+            msg = self.create_message(data)
+            server = self.setup_smtp_server()
+
+            # sending mail
+            server.sendmail(self.user_email, data.receiver_email_address, msg.as_string())
+            server.quit()
+
+            print("Email sent successfully")
+            return MailHandlerResponse(
+                success=True,
+                code=200,
+                status="Success"
+            )
+        except Exception as err:
+            print(f"Failed to sent message")
+            return MailHandlerResponse(
+                success=False,
+                code=200,
+                status="Failed",
+                error_message=str(err)
+            )
+
+    def create_message(self, data):
+        msg = MIMEMultipart()
+        msg["From"] = self.user_email
+        msg["To"] = data.receiver_email_address
+        msg["Subject"] = "subject"
+
+        msg.attach(MIMEText(data.message, "plain"))
+        return msg
+
+    def setup_smtp_server(self):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(self.user_email, "bpwy exxc nkkn eofg")
+        return server
